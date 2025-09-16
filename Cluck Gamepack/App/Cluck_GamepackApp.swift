31a88042eecbd17e5e20370f459c7cd1b50ea021
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-09-16 15:11:20 +0300

    init

---
+++ Cluck Gamepack/App/Cluck_GamepackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cluck_GamepackApp.swift
+//  Cluck Gamepack
+//
+//  Created by Serhii Babchuk on 16.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Cluck_GamepackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CGLaunchView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-Cluck Gamepack/Pods-Cluck Gamepack-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cluck_Gamepack : NSObject
+@end
+@implementation PodsDummy_Pods_Cluck_Gamepack
+@end
